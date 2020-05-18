a00c279b27f238e710c5627a9e124ba90b085c91
Severin Kilchhofer <>
2020-05-18 20:31:46 +0200

    initial commit

---
+++ components/Nav.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+export default function Nav() {
+    return (
+        <nav>
+            <ul className="flex justify-between items-center p-8">
+                <li>
+                    <Link href="/">
+                        <a className="text-blue-500 no-underline">Home</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/about">
+                        <a className="text-blue-500 no-underline">About</a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from "../components/Nav";
+
+const About = () => {
+    return (
+        <>
+            <Nav/>
+            <div className="hero">
+                <h1 className="title">About</h1>
+            </div>
+        </>
+    )
+}
+
+export default About
