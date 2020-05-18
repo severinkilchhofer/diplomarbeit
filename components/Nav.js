3c8efbec8682fe8ea5743d2d258571dd76a15f37
Severin Kilchhofer <>
2020-05-18 21:19:31 +0200

    added deploy command

--- components/Nav.js
+++ components/Nav.js
@@ -10,9 +10,7 @@ export default function Nav() {
                     </Link>
                 </li>
                 <li>
-                    <Link href="/about">
-                        <a className="text-blue-500 no-underline">About</a>
-                    </Link>
+                    <Link href={ '/about' } as={ process.env.BACKEND_URL + '/about' }><a>about</a></Link>
                 </li>
             </ul>
         </nav>
