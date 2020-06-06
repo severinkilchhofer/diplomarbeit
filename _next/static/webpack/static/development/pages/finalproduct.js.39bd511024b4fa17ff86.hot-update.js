9e5afa4ee380455bef9e52ff8092dcd28617e1ca
Severin Kilchhofer <>
2020-06-06 15:31:38 +0200

    Deploy Next.js to gh-pages

---
+++ _next/static/webpack/static/development/pages/finalproduct.js.39bd511024b4fa17ff86.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate("static/development/pages/finalproduct.js",[
+/* 0 */,
+/* 1 */
+false,
+/* 2 */,
+/* 3 */
+/*!********************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Ffinalproduct&absolutePagePath=%2FUsers%2Fseverinkilchhofer%2Fgit%2Fdiplomarbeit%2Fpages%2Ffinalproduct.js ***!
+  \********************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Ffinalproduct&absolutePagePath=%2FUsers%2Fseverinkilchhofer%2Fgit%2Fdiplomarbeit%2Fpages%2Ffinalproduct.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Ffinalproduct&absolutePagePath=%2FUsers%2Fseverinkilchhofer%2Fgit%2Fdiplomarbeit%2Fpages%2Ffinalproduct.js!./");
+
+
+/***/ })
+])
