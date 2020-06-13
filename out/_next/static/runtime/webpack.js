2a39e26852886fdd5e452149686f2f9dd9db2f43
Severin Kilchhofer <>
2020-06-13 12:26:35 +0200

    Deploy Next.js to gh-pages

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5f489e0896c2a6f065b6";
+/******/ 	var hotCurrentHash = "ffc6ae41cfb96851378e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
