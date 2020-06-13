066b07e8198f52f2d53e1f324fe4e1b1522f664b
Severin Kilchhofer <>
2020-06-13 12:25:51 +0200

    fixed small stylings

--- components/Rating/Steps/StepSicherheit.js
+++ components/Rating/Steps/StepSicherheit.js
@@ -9,7 +9,7 @@ const StepSicherheit = (props) => {
                 <div className="w-full sm:pt-4 lg:col-12 text-white">
                     <h1>Ist die Sicherheit und Gesundheit der Arbeitnehmer stets gewährleistet?</h1>
                     <div className="mt-1 sm:mt-8 flex flex-wrap">
-                        <label className="py-2 sm:px-2 w-full sm:w-auto">
+                        <label className="py-2 sm:px-2 w-full">
                             <input type="radio"
                                    defaultChecked={props.data.sicherheit === '10'}
                                    className="card-input-element"
@@ -20,7 +20,7 @@ const StepSicherheit = (props) => {
                                 <p>Ja sie ist jederzeit vollständig gewährleistet. Dazu dient beispielsweise das Tragen von Schutzkleidung.</p>
                             </div>
                         </label>
-                        <label className="py-2 sm:px-2 w-full sm:w-auto">
+                        <label className="py-2 sm:px-2 w-full">
                             <input type="radio"
                                    defaultChecked={props.data.sicherheit === '3'}
                                    className="card-input-element"
@@ -31,7 +31,7 @@ const StepSicherheit = (props) => {
                                 <p>Nein sie ist teilweise nicht gewährleistet. Die Arbeitnehmer sind teilweise gefährlichen Substanzen / Maschinen ausgesetzt, wobei es keine oder nur ungenügende Schutzmassnahmen gibt.</p>
                             </div>
                         </label>
-                        <label className="py-2 sm:px-2 w-full sm:w-auto">
+                        <label className="py-2 sm:px-2 w-full">
                             <input type="radio"
                                    defaultChecked={props.data.sicherheit === '1'}
                                    className="card-input-element"
